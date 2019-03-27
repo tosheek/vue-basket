9ce0dfcf6cf8616c5d551a477b7669d11208f966
tosheek <7123060@mail.ru>
2019-03-27 23:37:46 +0300

    Добавил сохранение корзины в localStorage, заголовок при пустой корзине, фикс стилей и названий переменных.

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 Vue.component("item", {
-  template: "#product-box",
-  props: ["item_data", "buyitems"],
+  template: "#item-box",
+  props: ["product_data", "basket_items"],
   methods: {
-    addItem: function(item_data) {
-      var i = this.findIndex(this.$parent.buyitems, "id", item_data.id);
+    addItem: function(product_data) {
+      var i = this.findIndex(this.$parent.basket_items, "id", product_data.id);
       if (i < 0) {
           this.pushData();        
       }else{
-          this.$parent.buyitems[i].qty += 1;
-          this.$parent.buyitems[i].total =this.$parent.buyitems[i].qty*this.$parent.buyitems[i].price;
+          this.$parent.basket_items[i].qty += 1;
+          this.$parent.basket_items[i].total =this.$parent.basket_items[i].qty*this.$parent.basket_items[i].price;
       }
+      this.$parent.save();
     },
     pushData: function() {
-      this.$parent.buyitems.push({
-        img: this.item_data.img,
-        title: this.item_data.title,
-        price: this.item_data.price,
+      this.$parent.basket_items.push({
+        img: this.product_data.img,
+        title: this.product_data.title,
+        price: this.product_data.price,
         qty: 1,
-        total: this.item_data.price,
-        id: this.item_data.id
+        total: this.product_data.price,
+        id: this.product_data.id
       });
     },
     findIndex: function(array, attr, value) {
@@ -32,18 +33,21 @@ Vue.component("item", {
   }
 });
 
-Vue.component("buyitem", {
+Vue.component("basket_item", {
   template: "#buy-box",
-  props: ["buy_data", "buyitems"],
+  props: ["buy_data", "basket_items"],
   methods: {
     removeItem: function(buy_data) {
-      var index = this.$parent.buyitems.indexOf(buy_data);
-      this.$parent.buyitems.splice(index, 1);
+      var index = this.$parent.basket_items.indexOf(buy_data);
+      this.$parent.basket_items.splice(index, 1);
+      
+      this.$parent.save();
     },
     plusQty: function(buy_data){
       buy_data.qty += 1;
       buy_data.total = buy_data.qty*buy_data.price;
-
+     
+      this.$parent.save();
     },
     minusQty: function(buy_data){
       buy_data.qty -= 1;
@@ -54,6 +58,8 @@ Vue.component("buyitem", {
 
       if (buy_data.qty == 0) 
         this.removeItem(buy_data);
+
+      this.$parent.save();
     }
     
   }
@@ -73,21 +79,39 @@ var app = new Vue({
         img: "https://chenyiya.com/codepen/product-3.jpg",title: "Paper Bag",price: "35",id: "paper-bag"
       }
     ],
-    buyitems: []
+    basket_items: []
   },
+  mounted() {
+    if (localStorage.getItem('basket_items')) {
+      try {
+        this.basket_items = JSON.parse(localStorage.getItem('basket_items'));
+      } catch(e) {
+        localStorage.removeItem('basket_items');
+      }
+    }
+  },
+  // watch:{
+  //   basket_items: function (val) {
+  //     this.save();
+  //   }
+  // }, 
   methods: {
+    save() {
+      const parsed = JSON.stringify(this.basket_items);
+      localStorage.setItem('basket_items', parsed);
+    },
+
     totalQty: function(){
       var qty = 0;
-      this.buyitems.forEach(function(buyitem){
-            //qty++;
-             qty += parseInt(buyitem.qty);
+      this.basket_items.forEach(function(basket_item){
+          qty += parseInt(basket_item.qty);
       });
       return qty;      
     },
-    total: function(){
+    getTotal: function(){
       var sum = 0;
-      this.buyitems.forEach(function(buyitem){
-            sum += parseInt(buyitem.total);
+      this.basket_items.forEach(function(basket_item){
+          sum += parseInt(basket_item.total);
       });
       return sum;
     }
